fb1e64a3fe5c2f169cfbb5d8d0990284e921a3d1
kevinarauz <kecin222294@gmail.com>
2021-10-17 21:43:14 -0500

    Se añade cliente observable

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
-import { CLIENTES } from './clientes.json';
+import { ClienteService } from './cliente.service';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
-  clientes: Cliente[]= [
-    { id: 1, nombre: 'kevin', apellido: 'arauz', email: 'kevin@gmail.com', createAt: '2021-07-17' },
-    { id: 2, nombre: 'leslie', apellido: 'arauz', email: 'liz@gmail.com', createAt: '2021-12-11' }
-  ];
-  constructor() { }
+
+  clientes: Cliente[];
+
+  constructor(private clienteService: ClienteService) { }
 
   ngOnInit(): void {
-    this.clientes= CLIENTES;
+    this.clienteService.getClientes().subscribe(
+       clientes => this.clientes = clientes
+    );
   }
 
 }
